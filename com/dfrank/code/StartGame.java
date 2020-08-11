5264689688f5284bbcb110ef3f0e1c3a1fe4b3a6
余月七 <63581729+Dfrank-wx@users.noreply.github.com>
2020-08-11 21:51:58 +0800

    Add files via upload

---
+++ com/dfrank/code/StartGame.java
@@ -0,0 +1,19 @@
+package com.dfrank.code;
+import javax.swing.JFrame;
+
+public class StartGame {
+
+    public static void main(String[] args) {
+        //1.新建一个窗口
+        JFrame frame = new JFrame("余月七——Java贪吃蛇小游戏");
+        frame.setBounds(10,10,900,720);  // 设置窗口的位置和大小
+        frame.setResizable(false); //窗口大小不可调整,即固定窗口大小
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 设置关闭事件，游戏可以关闭
+
+        //2.添加我们自己编写的画布背景
+        frame.add(new GamePanel());
+
+        frame.setVisible(true); //将窗口展示出来
+    }
+
+}
